667486112ada7e19b8bcdcf25e8858d6dff2e8b5
sanvine <santhiyamm2025@gmail.com>
2025-05-03 16:46:42 +0530

    Update Technologies.js

--- newpractice/Frontend/myapp/src/Subpages/Technologies.js
+++ newpractice/Frontend/myapp/src/Subpages/Technologies.js
@@ -6,22 +6,22 @@ return (
         <h2 class='tech-h2'>Our Technologies</h2>
         <div class='row'>
             <div class='col-6 col-sm-6 col-md-6 col-lg-2 my-3'>
-                <img src={'/images/tech1.jpg'} alt='' class='tech-img1'/>
+                <img src={'./Images/tech1.jpg'} alt='' class='tech-img1'/>
             </div>
             <div class='col-6 col-sm-6 col-md-6 col-lg-2 my-3'>
-                <img src={'/images/tech2.jpg'} alt='' class='tech-img1'/>
+                <img src={'./Images/tech2.jpg'} alt='' class='tech-img1'/>
             </div>
             <div class='col-6 col-sm-6 col-md-6 col-lg-2 my-3'>
-                <img src={'/images/tech3.jpg'} alt='' class='tech-img1'/>
+                <img src={'./Images/tech3.jpg'} alt='' class='tech-img1'/>
             </div>
             <div class='col-6 col-sm-6 col-md-6 col-lg-2 my-3'>
-                <img src={'/images/tech4.jpg'} alt='' class='tech-img1'/>
+                <img src={'./Images/tech4.jpg'} alt='' class='tech-img1'/>
             </div>
             <div class='col-6 col-sm-6 col-md-6 col-lg-2 my-3'>
-                <img src={'/images/tech5.jpg'} alt='' class='tech-img1'/>
+                <img src={'./Images/tech5.jpg'} alt='' class='tech-img1'/>
             </div>
             <div class='col-6 col-sm-6 col-md-6 col-lg-2 my-3'>
-                <img src={'/images/tech6.jpg'} alt='' class='tech-img1'/>
+                <img src={'./Images/tech6.jpg'} alt='' class='tech-img1'/>
             </div>
         </div>
     </div>
