19c9698a7210d08797d564f6f33afc2c32d815fe
sanvine <santhiyamm2025@gmail.com>
2025-05-03 16:42:51 +0530

    Update Courses.js

--- newpractice/Frontend/myapp/src/Pages/Courses.js
+++ newpractice/Frontend/myapp/src/Pages/Courses.js
@@ -19,7 +19,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/c.jpg'} alt='' />
+                            <img class='img-fluid w-100' src={'./Images/c.jpg'} alt='' />
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>C programming</h3>
                                 <p class='card-text mt-3'>Learn C programming with basic and core to build a strong foundation in computer science and software development</p>
@@ -28,7 +28,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/photoshop.jpg'} alt='' />
+                            <img class='img-fluid w-100' src={'./Images/photoshop.jpg'} alt='' />
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Adobe Photoshop</h3>
                                 <p class='card-text mt-3'>Adobe Photoshop is the industry-leading digital image editing and design application that offers the tools to create anything you can imagine.</p>
@@ -37,7 +37,7 @@ return (
                     </div>
                     <div class='col-12 col-md-6 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/tally.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/tally.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Tally</h3>
                                 <p class='card-text mt-3'>TallyPrime brings you a powerful reports dashboard, presenting your business information in intuitive visual formats</p>
@@ -46,7 +46,7 @@ return (
                     </div>
                     <div class='col-12 col-md-6 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/python.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/python.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Python</h3>
                                 <p class='card-text mt-3'>Python programming to build automate tasks efficiently. Learn key concepts basic and object-oriented programming</p>
@@ -55,7 +55,7 @@ return (
                     </div>
                     <div class='col-12 col-md-6 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/java.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/java.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Java</h3>
                                 <p class='card-text mt-3'>Learn Java to develop robust, platform-independent applications with basic and object-oriented principles.</p>
@@ -64,7 +64,7 @@ return (
                     </div>
                     <div class='col-12 col-md-6 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/c++.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/c++.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>C++</h3>
                                 <p class='card-text mt-3'>C++ to develop high-performance applications with basic, object-oriented and low-level programming techniques. </p>
@@ -73,7 +73,7 @@ return (
                     </div>
                     <div class='col-12 col-md-6 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/csh.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/csh.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>C#</h3>
                                 <p class='card-text mt-3'>Learn C# to build powerful applications for web, desktop, and mobile platforms using .NET.concepts like object-oriented programming </p>
@@ -82,7 +82,7 @@ return (
                     </div>
                     <div class='col-12 col-md-6 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/frontend.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/frontend.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Frontend Developer</h3>
                                 <p class='card-text mt-3'>Become a front-end developer by mastering HTML, CSS, and JavaScript to create interactive, user-friendly websites</p>
@@ -91,7 +91,7 @@ return (
                     </div>
                     <div class='col-12 col-md-6 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/sql.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/sql.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>SQL</h3>
                                 <p class='card-text mt-3'>SQL to efficiently manage and manipulate relational databases with powerful queries to work with large datasets</p>
@@ -100,7 +100,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/autocad.jpg'} height='200px' width='305px' alt='' />
+                            <img class='img-fluid w-100' src={'./Images/autocad.jpg'} height='200px' width='305px' alt='' />
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Autocad</h3>
                                 <p class='card-text mt-3'>AutoCAD software helps you easily edit, enhance, and maintain scanned drawings and plans in a AutoCAD environment. </p>
@@ -109,7 +109,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/devops.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/devops.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Devops</h3>
                                 <p class='card-text mt-3'>Devops is the combination of development and operations to improve the speed and reliability of software delivery </p>
@@ -118,7 +118,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/fullstack.png'} alt='' height='200px' width='305px'  />
+                            <img class='img-fluid w-100' src={'./Images/fullstack.png'} alt='' height='200px' width='305px'  />
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Full Stack Development</h3>
                                 <p class='card-text mt-3'>development of both frontend and backend portions of web applications with frameworks, libraries and databases</p>
@@ -127,7 +127,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/aws.jpg'} alt='' height='200px' width='305px'/>
+                            <img class='img-fluid w-100' src={'./Images/aws.jpg'} alt='' height='200px' width='305px'/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Amazon Cloud Services</h3>
                                 <p class='card-text mt-3'>Amazon Cloud Services offers reliable, scalable, and inexpensive cloud computing services and hands on practice.</p>
@@ -136,7 +136,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/selenium.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/selenium.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Selenium Automation</h3>
                                 <p class='card-text mt-3'>Web application testing using open-source tools and libraries across different browsers and operating systems. </p>
@@ -145,7 +145,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/datascience.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/datascience.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Data Science Python</h3>
                                 <p class='card-text mt-3'>principles and practices from the fields of mathematics, statistics, and computer engineering to analyze large amounts of data. </p>
@@ -154,7 +154,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/digital.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/digital.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Digital Marketing</h3>
                                 <p class='card-text mt-3'>Learn digital marketing to effectively promote brands and products across online platforms.Strategies like SEO, social media marketing </p>
@@ -163,7 +163,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/linux.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/linux.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Linux</h3>
                                 <p class='card-text mt-3'> Linux to efficiently manage systems, servers, and networks with open-source tools. Learn key concepts, shell scripting</p>
@@ -172,7 +172,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/mern.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/mern.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>MERN Stack</h3>
                                 <p class='card-text mt-3'> MERN stack (MongoDB, Express.js, React, Node.js) to build dynamic and scalable web applications with effective tools</p>
@@ -181,7 +181,7 @@ return (
                     </div>
                     <div class='col-12 col-md-4 col-lg-3 mt-5'>
                         <div class='card'>
-                            <img class='img-fluid w-100' src={'/images/jira.jpg'} height='200px' width='305px' alt=''/>
+                            <img class='img-fluid w-100' src={'./Images/jira.jpg'} height='200px' width='305px' alt=''/>
                             <div class='card-body mt-3'>
                                 <h3 class='card-title'>Jira</h3>
                                 <p class='card-text mt-3'> Jira is a popular project management tool used for tracking issues, bugs, and tasks, especially in agile software development.</p>
